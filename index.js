4700c28fb89287bf537ac1f825a3f34d8df9f732
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-03-23 02:38:28 -0300

    Commit for Heroku

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const professores = require("./data/professores.json")
 
 const PORT = 8082;
 
+app.get("/", (req, res) => res.send("Ok"));
+
 app.get("/professores", (req, res) => {
     res.json(professores);
 });
