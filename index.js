bb03510165c323f917972ad38869aa0c191e9134
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-03-23 03:10:25 -0300

    add end point alunos

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
-const professores = require("./data/professores.json")
+const professores = require("./data/professores.json");
+const alunos = require("./data/alunos.json");
 
 const PORT = 8082;
 
 app.get("/", (req, res) => res.send("Ok"));
 
-app.get("/professores", (req, res) => {
-    res.json(professores);
-});
+app.get("/professores", (req, res) => res.json(professores));
+
+app.get("/alunos", (req, res) => res.json(alunos));
 
 app.listen(process.env.PORT || PORT, () => console.log("Server running on port " + PORT));
